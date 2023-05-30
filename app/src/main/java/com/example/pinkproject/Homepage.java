d2bd4edc51935a9c2f1b9bae7432923df780b168
Rabiatul Adawiyah ZA <rabiatul.nraza@gmail.com>
2023-05-30 17:56:45 +0800

    navigation drawer

--- app/src/main/java/com/example/pinkproject/Homepage.java
+++ app/src/main/java/com/example/pinkproject/Homepage.java
@@ -1,19 +1,36 @@
 package com.example.pinkproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class Homepage extends AppCompatActivity {
 
-    Button categoryBtn,logoutBtn;
+    Button categoryBtn;
     public SignOut dialog;
-
+    View toolbar;
+    DrawerLayout drawerLayout;
+    LinearLayout homepageBtn,messageBtn,aboutBtn,updatePBtn;
+    TextView title;
+    ImageView logout,menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +40,6 @@ public class Homepage extends AppCompatActivity {
         dialog = new SignOut(this);
 
         categoryBtn = findViewById(R.id.categoryBtn);
-        logoutBtn = findViewById(R.id.logoutBtn);
 
 
         categoryBtn.setOnClickListener(new View.OnClickListener() {
@@ -34,10 +50,42 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
+
+        toolbar = findViewById(R.id.toolbar);
+        title = toolbar.findViewById(R.id.title);
+        logout = toolbar.findViewById(R.id.logoutBtn);
+        menu = toolbar.findViewById(R.id.menu);
+
+        dialog = new SignOut(this);
+
+        title.setText("Pink Project | HomePage");
+
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.show();
+            }
+        });
+
+        drawerLayout=findViewById(R.id.drawer);
+        homepageBtn = drawerLayout.findViewById(R.id.homepageBtn);
+        messageBtn = drawerLayout.findViewById(R.id.messageBtn);
+
+        homepageBtn.setBackgroundColor(ContextCompat.getColor(this,R.color.black));
+
+
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+
+        messageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                featureUnderProgress();
             }
         });
     }
@@ -53,6 +101,19 @@ public class Homepage extends AppCompatActivity {
             }
         });
         builder.setNegativeButton("No", null);
+        builder.show();
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+    public void featureUnderProgress(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Feature is under Progress");
+        builder.setNeutralButton("OK",null);
         builder.show();
     }
 }
